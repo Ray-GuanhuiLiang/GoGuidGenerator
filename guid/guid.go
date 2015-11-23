508aaec0d329e0c5187c650bad6318f5fa98d6c3
Ray-GuanhuiLiang <liangguanhui@qq.com>
2015-11-23 15:52:15 +0800

    init

---
+++ guid/guid.go
@@ -0,0 +1,5 @@
+package guid
+
+type Guid struct {
+	id int64
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Ray-GuanhuiLiang/GoGuidGenerator/guid"
+)
+
+func main() {
+	g := &guid.Guid{}
+	fmt.Println("guid")
+	fmt.Println(g)
+}
